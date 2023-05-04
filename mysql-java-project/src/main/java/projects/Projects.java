becc1accf0e66e9fd6f103b684d803183e4b632f
Jami <jamitvelez@gmail.com>
2023-05-04 10:12:42 -0700

    second commit

---
+++ mysql-java-project/src/main/java/projects/Projects.java
@@ -0,0 +1,18 @@
+package projects;
+
+import java.sql.Connection;
+import projects.dao.DbConnection;
+
+public class Projects {
+
+  public static void main(String[] args) {
+
+   DbConnection.getConnection(); 
+    
+    
+    
+    
+    
+  }
+
+}
